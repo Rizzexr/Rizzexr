a7377e9c1b3a08d3a583bb7eceff8d07be7be0d4
Rizzexr <149239565+Rizzexr@users.noreply.github.com>
2025-03-12 22:15:59 +0200

    Add files via upload

--- Zabroshki/script.js
+++ Zabroshki/script.js
@@ -90,11 +90,20 @@ const points = [
         difficulty: "1/3"
     },
     { 
-        lat: 46.445259, 
-        lon: 30.767850, 
+        lat: 46.4451444, 
+        lon: 30.767106, 
         label: "Аранжерея Маразли", 
         color: "#00FF09", 
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHtVjZ3fwwJpze8UrMHsZyAEC6P7KkaepPY-na-kxZuZiYrw_r0Ce5j19OQUDVIcpB7Fw&usqp=CAU",
+        guarded: "Нет",
+        difficulty: "1/3"
+    },
+    { 
+        lat: 46.445259, 
+        lon: 30.767850, 
+        label: "Корпус", 
+        color: "#00FF09", 
+        image: "https://nic-pnb.ru/wp-content/uploads/2014/09/no-foto-2.jpg",
         guarded: "Нет",
         difficulty: "1/3"
     },
@@ -133,7 +142,178 @@ const points = [
         image: "https://usionline.com/wp-content/uploads/2019/05/E9DA74EB-5C2F-46D3-BACD-70BFEDD6CE97-1510x848.jpeg",
         guarded: "Нет",
         difficulty: "1/3"
-    }
+    },
+    { 
+        lat: 46.373105, 
+        lon: 30.670442, 
+        label: "Недействующий резервуар", 
+        color: "#00FF09", 
+        image: "https://nic-pnb.ru/wp-content/uploads/2014/09/no-foto-2.jpg",
+        guarded: "Нет",
+        difficulty: "1/3"
+    },
+    { 
+        lat: 46.375842, 
+        lon: 30.729827, 
+        label: "Недостройка", 
+        color: "#00FF09", 
+        image: "https://nic-pnb.ru/wp-content/uploads/2014/09/no-foto-2.jpg",
+        guarded: "Нет",
+        difficulty: "1/3"
+    },
+    { 
+        lat: 46.370929, 
+        lon: 30.729970, 
+        label: "Дом Писателей", 
+        color: "#FF4444", 
+        image: "https://nic-pnb.ru/wp-content/uploads/2014/09/no-foto-2.jpg",
+        guarded: "Неизвестно",
+        difficulty: "3/3"
+    },
+    { 
+        lat: 46.435656, 
+        lon: 30.742883, 
+        label: "Заброшенная столовая", 
+        color: "#00FF09", 
+        image: "https://nic-pnb.ru/wp-content/uploads/2014/09/no-foto-2.jpg",
+        guarded: "Нет",
+        difficulty: "1/3"
+    },
+    { 
+        lat: 46.43867079054658, 
+        lon: 30.762995419371922, 
+        label: "Заброшенный корпус", 
+        color: "#00FF09", 
+        image: "https://nic-pnb.ru/wp-content/uploads/2014/09/no-foto-2.jpg",
+        guarded: "Нет",
+        difficulty: "1/3"
+    },
+    { 
+        lat: 46.4757702, 
+        lon: 30.6975442, 
+        label: "Заброшенные подземные склады (вход)", 
+        color: "#FFE500", 
+        image: "https://media-hosting.imagekit.io//44d6943df5ec4632/Screenshot_328.png?Expires=1836407957&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=dBIfU4cT7GUtfC~aUu4OZ5xwlfuanrB-lJNkiS5whfcpOjGHXC7zid0dUlYOESZd99xD8N~533j1o7-m16YeikhR1FFh3okfQOR8QZp5BaHEeeVhjctL5mgKuL-zw8d6iJPCooH2qNC7mQLs3lUw4FRvfciwI1gM-X4J4vMvHUSjLt8tNK32e2HFOhHWDYc1kzT0DHczG4NyQcuhJEduDip9h-KF-l9alUjYYqpk18EF5~Tpk8n4k7wX8gIy6Vlnazey1S3rVm5-p0sqrWKJaJ3WVQOwxj6L~Xl450tNDTFOLzauh5RJ-COq2DwV7kIRrqRAZk9RGVD9RrjXHXVpOg__",
+        guarded: "Нет",
+        difficulty: "2/3"
+    },
+    { 
+        lat: 46.4388964, 
+        lon: 30.7676927, 
+        label: "Корпус санатория", 
+        color: "#00FF09", 
+        image: "http://photos.wikimapia.org/p/00/05/27/34/38_1280.jpg",
+        guarded: "Нет",
+        difficulty: "1/3"
+    },
+    { 
+        lat: 46.4703775, 
+        lon: 30.713943, 
+        label: "Заброшенный подземный переход", 
+        color: "#FF4444", 
+        image: "http://photos.wikimapia.org/p/00/07/32/78/08_1280.jpg",
+        guarded: "Нет",
+        difficulty: "3/3"
+    },
+    { 
+        lat: 46.494419, 
+        lon: 30.7298685, 
+        label: "Судоремонтный завод в Одессе", 
+        color: "#FF4444", 
+        image: "http://photos.wikimapia.org/p/00/02/53/37/46_1280.jpg",
+        guarded: "Неизвестно",
+        difficulty: "3/3"
+    },
+    { 
+        lat: 46.5621053, 
+        lon: 30.7272265, 
+        label: "Заброшенная часть грязелечебницы", 
+        color: "#00FF09", 
+        image: "https://media-hosting.imagekit.io//32654f855ae64280/Screenshot_330.png?Expires=1836415277&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=zkxGn39bv716z3gIv1KHWLye0YJmL4i3OLnqNJexECAVM4WUYhSJbdIgOcAOabsDuul2IsIzHO~EBQAYO9Zbyz~4atOO5tE8cfhE23qmsnAtFHT0OZ26qd8OewuaOAL2Jx5tSNLu4qmfTrh~UdJwBsA4bfeFru8QgcXt5IKNbV8hNYQkjgMJcSZ2AS-2eDyb0s3T1GnkAjFx9HGoFYEDUJ1jFGp3F634BVx7YlXLTAnElsEyPRrMz9~8Lw4wemI-VvT3biFqRR-W7XrBOwJu~6GLjz3ZUTLAe2fGR4C4mLy4kVXBkgV2SRuQZhqILAHcpnL2EG46PlNdk3vz4hoUVw__",
+        guarded: "Нет",
+        difficulty: "1/3"
+    },
+    { 
+        lat: 46.539167, 
+        lon: 30.716944, 
+        label: "Клуб завода Большевик", 
+        color: "#00FF09", 
+        image: "http://photos.wikimapia.org/p/00/07/94/01/19_1280.jpg",
+        guarded: "Нет",
+        difficulty: "1/3"
+    },
+    { 
+        lat: 46.54386568520374, 
+        lon: 30.728901358155287, 
+        label: "Заброшенный кирпичный завод", 
+        color: "#00FF09", 
+        image: "http://photos.wikimapia.org/p/00/05/27/27/75_1280.jpg",
+        guarded: "Нет",
+        difficulty: "1/3"
+    },
+    { 
+        lat: 46.42125236635405,  
+        lon: 30.617096549451492, 
+        label: "Заброшка", 
+        color: "#00FF09", 
+        image: "https://nic-pnb.ru/wp-content/uploads/2014/09/no-foto-2.jpg",
+        guarded: "Нет",
+        difficulty: "1/3"
+    },
+    { 
+        lat: 46.317222, 
+        lon: 30.639722, 
+        label: "Бывший завод", 
+        color: "#FFE500", 
+        image: "https://nic-pnb.ru/wp-content/uploads/2014/09/no-foto-2.jpg",
+        guarded: "Неизвестно",
+        difficulty: "2/3"
+    },
+    { 
+        lat: 46.475000, 
+        lon: 30.737778, 
+        label: "ул. Европейская", 
+        color: "#FF4444", 
+        image: "http://photos.wikimapia.org/p/00/05/27/34/85_1280.jpg",
+        guarded: "Нет",
+        difficulty: "3/3"
+    },
+    { 
+        lat: 46.451723, 
+        lon: 30.768019, 
+        label: "Заброшка на берегу", 
+        color: "#FFE500", 
+        image: "https://nic-pnb.ru/wp-content/uploads/2014/09/no-foto-2.jpg",
+        guarded: "Вроде старый дед",
+        difficulty: "2/3"
+    },
+    { 
+        lat: 46.451944, 
+        lon: 30.756111, 
+        label: "Бывшая лаборатория физики алмазов", 
+        color: "#FF4444", 
+        image: "https://nic-pnb.ru/wp-content/uploads/2014/09/no-foto-2.jpg",
+        guarded: "Неизвестно",
+        difficulty: "3/3"
+    },
+    { 
+        lat: 46.505556, 
+        lon: 30.713889, 
+        label: "Руины на большой площади (СМУ-12)", 
+        color: "#00FF09", 
+        image: "https://nic-pnb.ru/wp-content/uploads/2014/09/no-foto-2.jpg",
+        guarded: "Нет",
+        difficulty: "1/3"
+    },
+    // { 
+    //     lat: , 
+    //     lon: , 
+    //     label: "", 
+    //     color: "#00FF09", 
+    //     image: "",
+    //     guarded: "Нет",
+    //     difficulty: "1/3"
+    // },
 ];
 
 // Функция для масштабирования маркеров в зависимости от зума
